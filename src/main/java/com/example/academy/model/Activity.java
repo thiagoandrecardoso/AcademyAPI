6195018e4cba734772c7a16dfbaa8a15cbc18eff
Thiago <thiagoandrecardoso@icloud.com>
2021-11-28 19:19:10 -0300

    Adicionado entidade Atividade, mas ainda não relacionada a entidade turma.

---
+++ src/main/java/com/example/academy/model/Activity.java
@@ -0,0 +1,54 @@
+package com.example.academy.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "activity")
+public class Activity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    private Date deadline;
+
+    private double maxScore;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public double getMaxScore() {
+        return maxScore;
+    }
+
+    public void setMaxScore(double maxScore) {
+        this.maxScore = maxScore;
+    }
+}
