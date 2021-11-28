380716fffad9bf02a81264459dfec8a36be8edbd
Thiago <thiagoandrecardoso@icloud.com>
2021-11-28 12:06:34 -0300

    Adicoonado métodos de buscar aluno por registro e salvar aluno.

--- src/main/java/com/example/academy/resources/StudentResource.java
+++ src/main/java/com/example/academy/resources/StudentResource.java
@@ -3,9 +3,7 @@ package com.example.academy.resources;
 import com.example.academy.model.Student;
 import com.example.academy.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,15 @@ public class StudentResource {
     @GetMapping("/list")
     public List<Student> getStudentList() {
         return studentRepository.findAll();
+    }
+
+    @GetMapping("/{registration}")
+    public Student getStudentById(@PathVariable(value="registration") long registration){
+        return studentRepository.findByRegistration(registration);
+    }
+
+    @PostMapping("/save")
+    public Student saveStudent(@RequestBody Student student){
+        return studentRepository.save(student);
     }
 }
