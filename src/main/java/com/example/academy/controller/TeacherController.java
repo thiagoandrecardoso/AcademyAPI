81c7505d7ce1d4686e7517c1af921e9760131ced
Thiago <thiagoandrecardoso@icloud.com>
2021-12-07 21:43:54 -0300

    ADICIONADO upload de arquivo para controller

--- src/main/java/com/example/academy/controller/TeacherController.java
+++ src/main/java/com/example/academy/controller/TeacherController.java
@@ -5,8 +5,12 @@ import com.example.academy.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TeacherController {
@@ -19,5 +23,12 @@ public class TeacherController {
         Teacher teacher = teacherRepository.findById(id);
         mv.addObject("teacher", teacher);
         return mv;
+    }
+
+    @PostMapping("/teacherOpenFile/upload") // //new annotation since 4.3
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        return "/";
     }
 }
